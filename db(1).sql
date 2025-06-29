f08557880159fac8e63ba939cd465722f1652e0d
gTonzig <144956377+gTonzig@users.noreply.github.com>
2025-06-29 12:30:06 +0200

    Update db.sql

--- db(1).sql
+++ db(1).sql
@@ -245,12 +245,11 @@ GROUP BY id_autista;
 -- -----------------------------------------------------
 -- Queries
 -- -----------------------------------------------------
--- 1) Semplice: elenco viaggi ancora aperti
+
 SELECT * 
 FROM `rideshare`.`viaggio`
 WHERE chiuso = FALSE;
 
--- 2) Intermedia: numero di prenotazioni accettate per viaggio
 SELECT
   v.id_viaggio,
   v.citta_partenza,
@@ -261,7 +260,6 @@ LEFT JOIN `rideshare`.`prenotazione` p
   ON v.id_viaggio = p.id_viaggio AND p.stato = 'ACCEPTED'
 GROUP BY v.id_viaggio;
 
--- 3) Difficile: per ciascun autista con ≥5 viaggi e voto medio ≥4.5, calcola tot. incasso e ordina
 SELECT
   sub.id_autista,
   sub.autista,
